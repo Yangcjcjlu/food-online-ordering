7e1bd5898d57428ec9e06c445af27f5743083b12
Yang <yangcjcjlu@126.com>
2019-01-11 00:58:21 +0800

    html页面路径配置

--- src/main/java/com/example/Controller/CustomerController.java
+++ src/main/java/com/example/Controller/CustomerController.java
@@ -18,10 +18,10 @@ public class CustomerController {
 
     @ResponseBody
     @RequestMapping("/select")
-    public String selectCustomer(Customer customer){
-        List<Customer> customerList= customserService.selectCustomer(customer);
-        for (Customer customer1:customerList
-             ) {
+    public String selectCustomer(Customer customer) {
+        List<Customer> customerList = customserService.selectCustomer(customer);
+        for (Customer customer1 : customerList
+                ) {
             System.out.println(customer1.getName());
 
         }
@@ -30,20 +30,54 @@ public class CustomerController {
 
     @ResponseBody
     @PostMapping("/insert")
-    public void insertCustomer(Customer customer){
-         this.customserService.insertCustomer(customer);
+    public void insertCustomer(Customer customer) {
+        this.customserService.insertCustomer(customer);
     }
 
 
     @RequestMapping(value = "/login")
-    public String Customer(){
-            return "login";
-        }
-
-    @RequestMapping(value="/index")
-    public String index(){return  "index";}
-
-    @RequestMapping(value = "/checkout")
-    public String checkout(){return "checkout";}
+    public String Customer() {
+        return "login";
     }
 
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/checkout")
+    public String checkout() {
+        return "checkout";
+    }
+
+
+    @RequestMapping(value = "/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping(value = "/m_register")
+    public String Mregister() {
+        return "Merchant register";
+    }
+
+    @RequestMapping(value = "/orders")
+    public String orders() {
+        return "orders";
+    }
+
+    @RequestMapping(value = "/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping(value = "/resturants")
+    public String resturants() {
+        return "resturants";
+    }
+
+    @RequestMapping(value = "/userInfo")
+    public String userInfo() {
+        return "Userinformation";
+    }
+}
