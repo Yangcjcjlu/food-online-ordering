c5330c5534c7043aa77345448b2d719c2f80c13d
Yang <yangcjcjlu@126.com>
2019-05-08 22:26:16 +0800

    food_online_ordering dishItem add and create shopping cart

--- src/main/java/com/example/Controller/OrdersController.java
+++ src/main/java/com/example/Controller/OrdersController.java
@@ -1,5 +1,6 @@
 package com.example.Controller;
 
+import com.example.Model.DishItem;
 import com.example.Model.Order;
 import com.example.Service.OrderService;
 import com.example.util.Result;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
@@ -31,6 +33,16 @@ public class OrdersController {
         this.orderService.OrderInsert(order);
         return Result.success();
     }
+
+//    @RequestMapping("/post")
+//    @ResponseBody
+//    public Object OrderInsert(HttpServletRequest request){
+//        System.out.println(request);
+//       // this.orderService.OrderInsert(order);
+//        return Result.success();
+//    }
+
+
 
     @RequestMapping("/delete")
     @ResponseBody
